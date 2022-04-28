023df2c818fb2984c251ed70ba7c80d1c0ef9105
Mohab <khalifamohab97@outlook.com>
2022-04-28 16:35:55 +0100

    added realAll function

---
+++ scripts/movies/read-movie.js
@@ -0,0 +1,38 @@
+(function() {
+    const requestSelector = document.querySelector('#method');
+    const dataTable = document.querySelector('#data-table');
+    const dataForm = document.querySelector('#data-form');
+    const movieIdField = document.querySelector('#movie-id-field');
+    const id = document.querySelector('#id');
+
+    function toggleIdVisibility(isVisible) {
+        if (isVisible) {
+            if (movieIdField.classList.contains('hide')) movieIdField.classList.remove('hide');
+        } else {
+            if (!movieIdField.classList.contains('hide')) movieIdField.classList.add('hide');
+        }
+    }
+
+    function readAll() {
+        setStatus('PREPARING GET REQUEST');
+
+        fetch('https://localhost:8080/movie', {
+            method: 'GET'
+        }).then(response => {
+            setStatus('RECEIVED RESPONSE');
+            if (response.ok) return response.json();
+            else throw new Error('Uh oh, something went wrong...');
+        })
+          .then(movies => {
+            setStatus('RENDERING TABLE');
+            renderMovieTable(movies, dataTable);
+            setStatus('RESPONSE RENDERED INTO TABLE');
+        })
+          .catch(error => {
+            setStatus('ERROR ENCOUNTERED');
+            handleError(error);
+        });
+    }
+
+
+})
